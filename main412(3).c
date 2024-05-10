5b1e5e712d4188e38cbe7dd4dce710431e815b51
Rwbo111 <18346634110@163.com>
2024-05-10 10:31:02 +0800

    Add files via upload

---
+++ main412(3).c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+    int total = 1; // 第10天的剩余桃子数量
+    int day;
+    for (day = 9; day >= 1; day--) {
+        total = (total + 1) * 2; // 前一天剩余桃子数量
+    }
+    printf("%d\n", total);
+    return 0;
+}
+
